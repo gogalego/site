ae691e450c2e90c5f6e8b7232b1f8129e6ea4e04
Gonçalo Galego <galego2320@hotmail.com>
2023-07-03 22:37:25 +0100

    Add Image Profile

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './home.css';
+import profileImage from '../assets/images/profile.png';
 
 const Home = () => {
   return (
     <div className="home-container">
       <div className="home-profile">
+        <div className='home-profile-description'>
+
+        </div>
+        <div className='home-profile-photo-container'>
+          <img  className='home-profile-photo' src={profileImage} alt="person"/>
+        </div>
       </div>
     </div>
   );
