6a21a5a3bd755be1efb04d63479bd0eda7d9a443
Gonçalo Galego <galego2320@hotmail.com>
2023-07-03 20:18:54 +0100

    Add Home Section & Css

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,30 @@ import { BrowserRouter } from 'react-router-dom';
 import { Layout } from 'antd';
 import './App.css';
 import About from './components/About';
-import Hero from './components/Hero'
+import Home from './components/Home';
 
 const { Header, Content, Footer } = Layout;
 
 function App() {
-
 
   return (
     <BrowserRouter>
       <Layout className='layout'>
       <Header className='header'>
         
-      <div className='logo-container'>
-        <img className='logo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-jR3imVo_HTPboBgj-9SE6REqz7y3wzaNtcY5r4Ml1Wj3iALlRaeEtF56sWulUJC4PSQ&usqp=CAU'></img>
-      </div>
+        <div className='logo-container'>
+          <img className='logo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-jR3imVo_HTPboBgj-9SE6REqz7y3wzaNtcY5r4Ml1Wj3iALlRaeEtF56sWulUJC4PSQ&usqp=CAU'></img>
+        </div>
 
-      <div className='anchor-container'>
-        <a className='anchor'>About</a>
-        <a className='anchor'>Projects</a>
-        <a className='anchor'>Contact</a>
-      </div>
+        <div className='anchor-container'>
+          <a className='anchor'>About</a>
+          <a className='anchor'>Projects</a>
+          <a className='anchor'>Contact</a>
+        </div>
       </Header>
       <Content>
 
-        <Hero />
+        <Home />
 
       </Content>
       <Footer
---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="home-profile">
+      </div>
+    </div>
+  );
+};
+
+export default Home;
