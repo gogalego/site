3ed30281a50ea0a04282ed6555f6ad586fa4c7c5
Gonçalo Galego <galego2320@hotmail.com>
2023-07-05 14:09:34 +0100

    Add Text About section & fix some css

--- src/App.tsx
+++ src/App.tsx
@@ -13,20 +13,11 @@ function App() {
     <BrowserRouter>
       <Layout className='layout'>
       <Header className='header'>
-        
-        <div className='logo-container'>
-          <img className='logo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-jR3imVo_HTPboBgj-9SE6REqz7y3wzaNtcY5r4Ml1Wj3iALlRaeEtF56sWulUJC4PSQ&usqp=CAU'></img>
-        </div>
-
-        <div className='anchor-container'>
-          <a className='anchor'>About</a>
-          <a className='anchor'>Projects</a>
-          <a className='anchor'>Contact</a>
-        </div>
       </Header>
       <Content>
 
         <Home />
+        <About />
 
       </Content>
       <Footer
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import './about.css';
 
 const About = () => {
   return (
     <div className="about-container">
-     
-    
+      <h1>About Me</h1>
+      <h2>I'm a Software Developer working from home</h2>
+      <p className='about-description'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis sapiente ipsum dolorum dicta eaque cumque inventore molestias, beatae ea quaerat alias accusamus voluptas autem! Alias odit voluptates in totam vitae dignissimos minus eaque culpa unde tempore dolore aperiam obcaecati voluptatum aliquam corrupti, suscipit accusamus! Odit unde veniam dolorum ipsum doloribus.</p>
       
     </div>
   );
